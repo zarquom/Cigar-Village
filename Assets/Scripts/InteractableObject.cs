d68eaad147e5a55f71e3fc52c925af2d391967d2
Marcos Paulis <moonspell.doom@gmail.com>
2026-07-16 21:03:47 +0200

    Implement cigar spawn in well

--- Assets/Scripts/InteractableObject.cs
+++ Assets/Scripts/InteractableObject.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.Localization;
@@ -6,15 +7,44 @@ public class InteractableObject : MonoBehaviour
 {
     private bool canInteract = true;
     [SerializeField] private LocalizedString interactionPromptTerm;
+    [SerializeField] private InteractionType interactionType;
+    [SerializeField] private GameObject interactionPrefab;
+    [SerializeField] private Transform interactionPrefabTransform;
     public bool CanInteract => canInteract;
     public LocalizedString InteractionPromptTerm => interactionPromptTerm;
+    public InteractionType InteractionType => interactionType;
+    public GameObject InteractionPrefab => interactionPrefab;
     public void Interact()
     {
         if (canInteract)
         {
-            // Perform interaction logic here
+            PerformInteraction();
             Debug.Log($"Interacted with {gameObject.name}");
             canInteract = false; // Prevent further interactions
         }
     }
+
+    private void PerformInteraction()
+    {
+        switch( interactionType ) {
+            case InteractionType.Pickup:
+                GameObject obj = Instantiate( interactionPrefab, transform );
+                obj.transform.localPosition = new Vector3(-1.44f, -0.8f, 0.65f);
+                break;
+            case InteractionType.Talk:
+                // Handle talk interaction
+                break;
+            case InteractionType.Examine:
+                // Handle examine interaction
+                break;
+        }
+    }
+}
+
+public enum InteractionType
+{
+    None,
+    Pickup,
+    Talk,
+    Examine
 }
