b32624c7bd2b5002c54f0fe789b36b9f47aa5b56
Marcos Paulis <moonspell.doom@gmail.com>
2026-07-18 11:42:02 +0200

    Add movement and smoke effect to cigar

--- Assets/Scripts/CigarObject.cs
+++ Assets/Scripts/CigarObject.cs
@@ -1,7 +1,14 @@
+using DG.Tweening;
 using UnityEngine;
 
 public class CigarObject : MonoBehaviour
 {
     [SerializeField] private float cigarValue = 1f;
+    [SerializeField] private GameObject cigarObject;
     public float CigarValue => cigarValue;
+
+    private void Start()
+    {
+        cigarObject.transform.DOLocalMoveZ(-1.6f, 1f).SetLoops(-1, LoopType.Yoyo);
+    }
 }
